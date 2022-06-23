a8643cae876d5550571d74133c3a60d57aa11814
Katie-Rey <katiejorey@gmail.com>
2022-06-23 19:32:19 +1200

    Setup auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/userControllers')
+const { registerUser, loginUser, getMe } = require('../controllers/userControllers')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
+router.get('/me', getMe)
 
 module.exports = router
